0021a9dfa202971402319481e7b61aaf60531945
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-01-14 15:03:15 +0530

    locales made non-mandatory

--- typings/defaults.d.ts
+++ typings/defaults.d.ts
@@ -4,7 +4,7 @@
 * MIT Licensed
 */
 export declare const config: {
-    'contentstack': {
+    contentstack: {
         MAX_RETRY_LIMIT: number;
         actions: {
             delete: string;
@@ -22,6 +22,7 @@ export declare const config: {
             get: string;
         };
     };
+    locales: any[];
     'sync-manager': {
         cooloff: number;
         limit: number;
