a1a80d5f90c3a8dbf67c1b4273536c14b5158472
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-02-18 15:26:01 +0530

    sync notifications support added

--- example/index.js
+++ example/index.js
@@ -16,3 +16,17 @@ syncManager.setConfig(config)
 syncManager.start().then(() => {
   console.log('Sync utility started successfully!')
 }).catch(console.error)
+
+syncManager.notifications
+  .on('publish', (obj) => {
+    console.log('SYNC-PUBLISH: ', obj)
+  })
+  .on('unpublish', (obj) => {
+    console.log('SYNC-UNPUBLISH: ', obj)
+  })
+  .on('delete', (obj) => {
+    console.log('SYNC-DELETE: ', obj)
+  })
+  .on('error', (obj) => {
+    console.log('SYNC-ERROR: ', obj)
+  })
