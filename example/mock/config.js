d2cc879c34cf72326a3b92bc51793a8debb8578c
iyerrama29 <iyerrama29+contentstack.github@gmail.com>
2019-04-05 15:20:24 +0530

    inet bugs fixed, network retries + sync filters tested

--- example/mock/config.js
+++ example/mock/config.js
@@ -12,6 +12,18 @@ module.exports = {
     dbName: 'markdown',
     collectionName: 'development'
   },
+  syncManager: {
+    inet: {
+      dns: '8.8.8.8',
+      host: 'google.com',
+      port: 53,
+      retries: 2,
+      retryTimeout: 3 * 1000,
+      retryIncrement: 1 * 1000,
+      timeout: 6 * 1000,
+      type: 'A',
+    }
+  }
   // filtering now available!
   // syncManager: {
   //   filters: {
