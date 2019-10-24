664b42fd1342e2669eaaed903b4f555d502fd359
asmit-patil <patilasmit0gmail.com>
2019-10-24 17:34:13 +0530

    updated helper functions for modualr block within modular block

--- src/plugins/helper.js
+++ src/plugins/helper.js
@@ -113,7 +113,7 @@ exports.hasRteOrMarkdown = (schema) => {
           }
           continue
         } else if (field && field.data_type === 'blocks' && field.blocks) {
-          for (let x = 0, y = field.blocks; x < y; x++) {
+          for (let x = 0, y = field.blocks.length; x < y; x++) {
             if (this.hasRteOrMarkdown(field.blocks[x].schema)) {
               return true
             }
@@ -151,7 +151,7 @@ const checkReferences = (schema, key) => {
           }
           continue
         } else if (field && field.data_type === 'blocks' && field.blocks) {
-          for (let x = 0, y = field.blocks; x < y; x++) {
+          for (let x = 0, y = field.blocks.length; x < y; x++) {
             if (checkReferences(field.blocks[x].schema, key)) {
               return true
             }
