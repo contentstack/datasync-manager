a38c9a2d887c5bcfda789fb608314ef789e40d8f
Dinesh <dineshg.gowda@contentstack.com>
2019-10-29 17:09:27 +0530

    removed credentials

--- dist/config.js
+++ dist/config.js
@@ -43,7 +43,7 @@ exports.config = {
             content_types: '/v3/content_types/',
             sync: '/v3/stacks/sync',
         },
-        host: 'dev6-new-api.contentstack.io',
+        host: 'cdn.contentstack.io',
         options: 'ig',
         port: 443,
         protocol: 'https:',
