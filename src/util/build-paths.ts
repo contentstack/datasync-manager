9a88a7371dc9019316ec3ab21b2f36f968d21c9b
Dinesh <dineshg.gowda@contentstack.com>
2019-10-29 17:09:27 +0530

    removed credentials

--- src/util/build-paths.ts
+++ src/util/build-paths.ts
@@ -11,8 +11,7 @@ import { isAbsolute, join, resolve } from 'path'
  * @returns {Object} Returns config paths
  */
 export const buildConfigPaths = () => {
-  // const baseDir = resolve(join(__dirname, '..', '..', '..', '..'))
-  const baseDir = resolve(join(__dirname, '..'))
+  const baseDir = resolve(join(__dirname, '..', '..', '..', '..'))
   let pluginPath: string
   let tokenPath: string
 
