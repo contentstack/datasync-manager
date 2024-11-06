55b809b0de169eb00b099d140e15f3099cb4e699
raj pandey <raj.pandey@contentstack.com>
2024-11-06 13:59:45 +0530

    Fixed the semgrep issue

--- src/plugins/helper.js
+++ src/plugins/helper.js
@@ -97,7 +97,12 @@ const update = (parent, reference, entry) => {
           }
         }
       } else {
-        entry = entry[parent[j]]
+        const key = _parent[j];
+        if (Object.prototype.hasOwnProperty.call(entry, key)) {
+          const tempEntry = Object.create(null);
+          _.merge(tempEntry, entry);
+          entry = tempEntry[key];
+        }
         const keys = cloneDeep(parent).splice((j + 1), len)
         if (Array.isArray(entry)) {
           for (let i = 0, l = entry.length; i < l; i++) {
